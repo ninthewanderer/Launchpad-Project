a7af592b34b0a0e363c0e44bc89cfec2ed97edb4
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2026-03-27 19:25:13 -0400

    Fixed Camera

--- LP Unity Project/Assets/Scripts/BaseMovement.cs
+++ LP Unity Project/Assets/Scripts/BaseMovement.cs
@@ -1,58 +1,126 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float speed = 5f;
+    public float turnSpeed = 10f;
+
+    [Header("Jump")]
     public float jumpForce = 10f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
+    [Header("Camera/Look")]
+    public Transform cameraTarget;
+    public float cameraYOffset = 1.5f;
+    public float mouseSensitivity = 200f;
+    public float controllerSensitivity = 150f;
+    public float minPitch = -30f;
+    public float maxPitch = 60f;
+
     private Rigidbody rb;
     private bool isGrounded;
-
+    private float yaw;
+    private float pitch;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool jumpPressed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
 
+        Cursor.lockState = CursorLockMode.Locked;
+
+        yaw = transform.eulerAngles.y;
+        pitch = 0f;
+    }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
-        transform.Translate(movement);
-
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0)) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            Debug.Log("Jumped");
-        }
+        ReadInput();
+        HandleJump();
+        UpdateCameraTarget();
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
-        if (isGrounded)
-        {
-            Debug.Log("Grounded");
-        }
-        else
-        {
-            Debug.Log("Not Grounded");
-        }
+        CheckGround();
+        MovePlayer();
+        RotatePlayer();
     }
-    void OnDrawGizmosSelected()
+
+    void ReadInput()
     {
-        if (groundCheck != null)
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        float stickX = 0f;
+        float stickY = 0f;
+        try
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            stickX = Input.GetAxis("RightStickX") * controllerSensitivity * Time.deltaTime;
+            stickY = Input.GetAxis("RightStickY") * controllerSensitivity * Time.deltaTime;
+        }
+        catch { }
+
+        yaw += mouseX + stickX;
+        pitch -= mouseY + stickY;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+    }
+
+
+    void MovePlayer()
+    {
+        
+        Vector3 moveDir = new Vector3(horizontalInput, 0f, verticalInput);
+        Vector3 horizontalVelocity = moveDir.normalized * speed;
+        horizontalVelocity = transform.TransformDirection(horizontalVelocity);
+
+       
+        Vector3 newVelocity = new Vector3(horizontalVelocity.x, rb.velocity.y, horizontalVelocity.z);
+        rb.velocity = newVelocity;
+    }
+
+    void RotatePlayer()
+    {
+        Quaternion targetRot = Quaternion.Euler(0f, yaw, 0f);
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRot, turnSpeed * Time.fixedDeltaTime));
+    }
+
+
+    void HandleJump()
+    {
+        jumpPressed = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0);
+        if (jumpPressed && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
     }
 
+    void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+   
+    void UpdateCameraTarget()
+    {
+        if (cameraTarget == null) return;
+
+        
+        Vector3 targetPos = transform.position + Vector3.up * cameraYOffset;
+        cameraTarget.position = Vector3.Lerp(cameraTarget.position, targetPos, 10f * Time.deltaTime);
+
+        
+        cameraTarget.rotation = Quaternion.Euler(pitch, yaw, 0f);
+    }
 }
