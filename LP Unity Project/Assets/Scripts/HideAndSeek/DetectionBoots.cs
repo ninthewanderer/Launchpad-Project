ddab0881028353f772f62d4e68b4836884c77341
Chandler Guzman <ninthewander@gmail.com>
2026-04-10 13:00:50 -0400

    detection boots wip

--- LP Unity Project/Assets/Scripts/HideAndSeek/DetectionBoots.cs
+++ LP Unity Project/Assets/Scripts/HideAndSeek/DetectionBoots.cs
@@ -1,41 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DetectionBoots : MonoBehaviour
 {
     public HideAndSeekManager hnsManager;
-    public float detectionRadius;
     public LayerMask catTraces;
-    
-    private GameObject player;
-    private GameObject[] randomPath; // may not need.
+    public float detectionRadius;
+    public float disappearTime;
+    public float abilityCooldown;
+
     private String pathName;
-    
-    
+    private bool onCooldown = false;
+
     void Start()
     {
-        // Obtains the path picked by the Hide & Seek Manager.
-        randomPath = hnsManager.GivePath(); // may not need.
+        // Obtains the name of the path picked by the Hide & Seek Manager.
         pathName = hnsManager.GivePathName();
+        StartCoroutine(CooldownCheck());
     }
-    
+
     void Update()
     {
         // E on keyboard, Y on controller
         // This activates the detection boots ability.
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button3))
+        if (!onCooldown && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button3)))
         {
-            Debug.Log("Checking for traces.");
+            onCooldown = true;
             CheckForTraces();
+        }
+        else if (onCooldown && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button3)))
+        {
+            Debug.Log("Ability is on cooldown. Please wait " + abilityCooldown + " seconds.");
         }
     }
 
+    // Checks for traces of the cat near the player.
     private void CheckForTraces()
     {
-        Collider[] traces = Physics.OverlapSphere(transform.position, detectionRadius, catTraces, QueryTriggerInteraction.Collide);
+        Collider[] traces = Physics.OverlapSphere(transform.position, detectionRadius, catTraces,
+            QueryTriggerInteraction.Collide);
 
         if (traces.Length != 0)
         {
@@ -46,12 +51,32 @@ public class DetectionBoots : MonoBehaviour
                 {
                     int defaultLayer = LayerMask.NameToLayer("Default");
                     traceObj.layer = defaultLayer;
-                    
-                    // FIXME: revert it back to its previous layer after a set amount of time. (coroutine)
-                    // ensure player has detection boot cooldown
-                    Debug.Log("Trace detected!");
+                    StartCoroutine(TraceDisappear(traceObj));
                 }
             }
+        }
+    }
+
+    // Makes the traces of the cat disappear after a specified amount of time.
+    private IEnumerator TraceDisappear(GameObject traceObj)
+    {
+        yield return new WaitForSeconds(disappearTime);
+        int traceLayer = LayerMask.NameToLayer("Cat_Traces");
+        traceObj.layer = traceLayer;
+    }
+
+    // Manages the ability cooldown for the boots.
+    private IEnumerator CooldownCheck()
+    {
+        while (true)
+        {
+            if (onCooldown)
+            {
+                yield return new WaitForSeconds(abilityCooldown);
+                Debug.Log("Ability can now be used.");
+                onCooldown = false;
+            }
+            yield return null;
         }
     }
 }
