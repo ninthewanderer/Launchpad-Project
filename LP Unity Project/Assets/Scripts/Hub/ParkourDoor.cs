1e3a441eb10422a1cc21e64bead9d1a628e0966c
Summer Smith <summersmith2676@gmail.com>
2026-04-12 17:18:12 -0400

    fix collider in parkour lvl

--- LP Unity Project/Assets/Scripts/Hub/ParkourDoor.cs
+++ LP Unity Project/Assets/Scripts/Hub/ParkourDoor.cs
@@ -9,7 +9,7 @@ public class ParkourDoor : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("BrandonWork");
+            SceneManager.LoadScene("LvlPlatforming");
         }
     }
 }
