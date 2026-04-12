7ebc85b88e67f215436c2f39fc6c40723bc7ac73
Summer Smith <summersmith2676@gmail.com>
2026-04-12 19:56:46 -0400

    Revert "Merge branch 'art'"
    This reverts commit 2c550b5d5a5459a557bae851cd56b87cb455d51e, reversing
    changes made to 9a169074f9aa8114cf0bf77df22d8efc761b695b.

--- LP Unity Project/Assets/Scripts/UI/MainMenu.cs
+++ LP Unity Project/Assets/Scripts/UI/MainMenu.cs
@@ -20,9 +20,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
-
-    public void LoadHub()
-    {
-        SceneManager.LoadScene("LvlHub");
-    }
 }
