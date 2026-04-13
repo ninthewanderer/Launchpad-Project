ca039510d148a0c1bcf4e9cd9a537c7dabbbcaab
Summer Smith <summersmith2676@gmail.com>
2026-04-12 23:02:05 -0400

    Merge

--- LP Unity Project/Assets/Scripts/HideAndSeek/DetectionBoots.cs
+++ LP Unity Project/Assets/Scripts/HideAndSeek/DetectionBoots.cs
@@ -43,6 +43,7 @@ public class DetectionBoots : MonoBehaviour
             onCooldown = true;
             SetCharge(-chargeLost);
             CheckForTraces();
+            GetComponent<PlayerSounds>()?.PlayDetectionBootsSound();
         }
     }
 
