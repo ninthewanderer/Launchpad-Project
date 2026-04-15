d8144d0b8f423e5686de29481ccbb686de90d917
Chandler Guzman <ninthewander@gmail.com>
2026-04-15 12:31:12 -0400

    added script for motion-activated doors!
    for the tutorial mostly but we can use it later

--- LP Unity Project/Assets/Scripts/OpenDoor.cs
+++ LP Unity Project/Assets/Scripts/OpenDoor.cs
@@ -5,35 +5,73 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject door;
-    public Transform endPosition;
-    public float smoothTime = 15;
-    public  Vector3 velocity = new Vector3(0,0,2);
-    private bool doorOpen;
+    public GameObject end;
+    public float moveSpeed;
+    private bool doorOpen = true;
+    private bool doorClosing;
+    private Vector3 startPosition;
 
+    void Start()
+    {
+        // Stores the starting position for MoveDoorDown();
+        startPosition = door.transform.position;
+    }
+    
+    void Update()
+    {
+        // If the door isn't open, it will move up. If it is closing, the door will move back to its starting position.
+        if (!doorOpen)
+        {
+            MoveDoorUp();
+        }
+        
+        if (doorClosing)
+        {
+            MoveDoorDown();
+        }
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
+        // When the player enters the collider, the door will start moving up.
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(MoveDoor());
+            doorOpen = false;
+            doorClosing = false;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        // When the player exits the collider, the door will start moving back down.
         if (other.CompareTag("Player"))
         {
-            doorOpen = false;
+            doorClosing = true;
+            doorOpen = true;
         }
     }
 
-    private IEnumerator MoveDoor()
+    private void MoveDoorUp()
     {
-        Vector3 endPos = endPosition.position;
-        while (!doorOpen)
+        // Moves the door up and adjusts the doorOpen boolean after.
+        door.transform.position = Vector3.MoveTowards(door.transform.position, end.transform.position, 
+            moveSpeed * Time.deltaTime);
+
+        if (door.transform.position == end.transform.position)
         {
-            door.transform.position = Vector3.SmoothDamp(door.transform.position, endPos, ref velocity, smoothTime);
-            yield return new WaitWhile(() => door.transform.position != endPos);
             doorOpen = true;
+        }
+    }
+
+    private void MoveDoorDown()
+    {
+        // Moves the door up and adjusts the doorClosing boolean after.
+        door.transform.position = Vector3.MoveTowards(door.transform.position, startPosition, 
+            moveSpeed * Time.deltaTime);
+
+        if (door.transform.position == startPosition)
+        {
+            doorClosing = false;
         }
     }
 }
