abf9fda51520cd03fa193e20f263d2b4e8077423
ElleDeRoin <ellederoin@gmail.com>
2026-04-25 18:35:05 -0500

    SaveData

--- LP Unity Project/Assets/Scripts/SaveData.cs
+++ LP Unity Project/Assets/Scripts/SaveData.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaveData : MonoBehaviour
 {
@@ -7,6 +9,18 @@ public class SaveData : MonoBehaviour
     public bool HideAndSeekComplete { get; private set; }
     public bool PlatformingComplete  { get; private set; }
     public bool PuzzleComplete       { get; private set; }
+    public bool TutorialComplete     { get; private set; }
+
+    private Image hideAndSeekCheckmark;
+    private Image platformingCheckmark;
+    private Image puzzleCheckmark;
+    private Image tutorialCheckmark;
+
+    [Header("Task Checkmark Object Names")]
+    [SerializeField] private string hideAndSeekCheckmarkName = "HideAndSeekCheckmark";
+    [SerializeField] private string platformingCheckmarkName  = "PlatformingCheckmark";
+    [SerializeField] private string puzzleCheckmarkName       = "PuzzleCheckmark";
+    [SerializeField] private string tutorialCheckmarkName     = "TutorialCheckmark";
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void CreateInstance()
@@ -18,15 +32,77 @@ public class SaveData : MonoBehaviour
 
     void Awake()
     {
-
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
             return;
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    public void CompleteHideAndSeek() => HideAndSeekComplete = true;
-    public void CompletePlatforming()  => PlatformingComplete  = true;
-    public void CompletePuzzle()       => PuzzleComplete       = true;
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        RegisterCheckmarks();
+    }
+
+    private void RegisterCheckmarks()
+    {
+        hideAndSeekCheckmark = FindImageByName(hideAndSeekCheckmarkName);
+        platformingCheckmark  = FindImageByName(platformingCheckmarkName);
+        puzzleCheckmark       = FindImageByName(puzzleCheckmarkName);
+        tutorialCheckmark     = FindImageByName(tutorialCheckmarkName);
+
+        RefreshCheckmarks();
+    }
+
+    private Image FindImageByName(string objectName)
+    {
+        GameObject obj = GameObject.Find(objectName);
+        if (obj == null) return null;
+        return obj.GetComponent<Image>();
+    }
+
+    public void CompleteHideAndSeek()
+    {
+        HideAndSeekComplete = true;
+        SetCheckmark(hideAndSeekCheckmark, true);
+    }
+
+    public void CompletePlatforming()
+    {
+        PlatformingComplete = true;
+        SetCheckmark(platformingCheckmark, true);
+    }
+
+    public void CompletePuzzle()
+    {
+        PuzzleComplete = true;
+        SetCheckmark(puzzleCheckmark, true);
+    }
+
+    public void CompleteTutorial()
+    {
+        TutorialComplete = true;
+        SetCheckmark(tutorialCheckmark, true);
+    }
+
+    private void SetCheckmark(Image checkmark, bool isVisible)
+    {
+        if (checkmark != null)
+            checkmark.enabled = isVisible;
+    }
+
+    public void RefreshCheckmarks()
+    {
+        SetCheckmark(hideAndSeekCheckmark, HideAndSeekComplete);
+        SetCheckmark(platformingCheckmark,  PlatformingComplete);
+        SetCheckmark(puzzleCheckmark,       PuzzleComplete);
+        SetCheckmark(tutorialCheckmark,     TutorialComplete);
+    }
 }
