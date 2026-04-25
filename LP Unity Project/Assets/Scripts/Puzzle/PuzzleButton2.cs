a6367d0290e77c9b1cc7be56852e2d0f72165220
Summer Smith <summersmith2676@gmail.com>
2026-04-25 18:07:44 -0400

    add button visual changes

--- LP Unity Project/Assets/Scripts/Puzzle/PuzzleButton2.cs
+++ LP Unity Project/Assets/Scripts/Puzzle/PuzzleButton2.cs
@@ -8,6 +8,8 @@ public class PuzzleButton2 : MonoBehaviour
 {
     public GameObject[] affectedObjects;
     public Material magneticMaterial;
+    public Material emissiveRed;
+    public Material nonemissiveRed;
     public GameObject ConnectedButton;
     public bool timeLimit = false;
     public float timeLimitDuration = 5f;
@@ -89,6 +91,20 @@ public class PuzzleButton2 : MonoBehaviour
                     }
                 }
             }
+            foreach (Transform child in transform)
+            {
+                Debug.Log("Swapping material for child: " + child.name);
+                Material[] materials = child.GetComponent<Renderer>().materials;
+                for (int i = 0; i < materials.Length; i++)
+                {
+                    if (materials[i].name.Contains("red"))
+                    {
+                        int indexToSwitch = i;
+                        materials[indexToSwitch] = nonemissiveRed;
+                    }
+                }
+                child.GetComponent<Renderer>().materials = materials;
+            }
         }
     }
 
@@ -97,6 +113,19 @@ public class PuzzleButton2 : MonoBehaviour
         yield return new WaitForSeconds(timeLimitDuration);
         isPressed = false;
         Debug.Log("Time limit expired, button can be pressed again.");
+            foreach (Transform child in transform)
+                {
+                    Material[] materials = child.GetComponent<Renderer>().materials;
+                    for (int i = 0; i < materials.Length; i++)
+                    {
+                        if (materials[i].name.Contains("red"))
+                        {
+                            int indexToSwitch = i;
+                            materials[indexToSwitch] = emissiveRed;
+                        }
+                    }
+                    child.GetComponent<Renderer>().materials = materials;
+        }
     }
     // "Enables" magnetic platforms by setting them to the right layer.
     private void EnableMagneticPlatform(GameObject affectedObject)
