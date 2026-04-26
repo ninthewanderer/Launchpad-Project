44d58500ff4010181a80a4433f3996d0c7b2a238
Chandler Guzman <ninthewander@gmail.com>
2026-04-26 13:32:07 -0400

    nvm deleting new input system

---
+++ LP Unity Project/Assets/UIControllerInput.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerInput : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            
+        }
+    }
+}
